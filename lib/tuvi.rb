f136b22e01982be1146587bde6f7b5e6b6707668
Jay Wengrow <jaywngrw@gmail.com>
2013-08-21 15:09:35 -0500

    refactored the run method

--- lib/tuvi.rb
+++ lib/tuvi.rb
@@ -11,16 +11,26 @@ module Tuvi
   def run
     current_step = 1
     while true do
-      puts @steps[current_step].get_message
-      exit if @steps[current_step].exit_program
-      puts @steps[current_step].formatted_answers
-      input = gets.downcase.chomp
-      if @steps[current_step].answer_paths[input]
-        current_step = @steps[current_step].answer_paths[input]
-      else
-        puts "Sorry, I don't understand that answer. Please try again:"
-      end
+      current_step = execute_step(current_step)
     end
+  end
+
+  def execute_step(current_step)
+    puts @steps[current_step].get_message
+    exit if @steps[current_step].exit_program
+    puts @steps[current_step].formatted_answers
+    determine_next_step(current_step)
+  end
+
+  def determine_next_step(current_step)
+    input = gets.downcase.chomp
+    if @steps[current_step].answer_paths[input]
+      next_step = @steps[current_step].answer_paths[input]
+    else
+      puts "Sorry, I don't understand that answer. Please try again:"
+      next_step = current_step
+    end
+    next_step
   end
 
 end
